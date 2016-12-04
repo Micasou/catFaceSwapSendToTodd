f130b192c14990a491bf1c5c4ebfb9d8c6caf30c
RhaydenX <alamb25@uw.edu>
2016-12-03 19:29:38 -0800

    Update SendMessage.py

--- SendMessage/SendMessage.py
+++ SendMessage/SendMessage.py
@@ -5,7 +5,7 @@ AUTH_TOKEN = "591d13c644fc27af276d972990a822d4"
 def sendMessage(mediaURL):
     client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
     client.messages.create(
-        to="+13605359123", 
+        to="+14254298436", 
         from_="+19284874422 ", 
         media_url=mediaURL, 
     )    
