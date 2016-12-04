ad5d71380befd92407ebfdea76c430f730fc4e83
Max Wenger <carefree4@gmail.com>
2016-12-03 18:23:11 -0800

    Added UI fraimwork and hello world home page. Can get filepath and phone number

--- FaceSwap/Window.py
+++ FaceSwap/Window.py
@@ -1,8 +1,33 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
-class Window(App):
+
+class Window(GridLayout):
+
+    def send(instance, value):
+        print(instance.filePath.text + ' ' +instance.phoneNumber.text)
+
+    def __init__(self, **kwargs):
+        super(Window, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='File Path'))
+        self.filePath = TextInput(multiline=False)
+        self.add_widget(self.filePath)
+        self.add_widget(Label(text='phoneNumber'))
+        self.phoneNumber = TextInput(multiline=False)
+        self.add_widget(self.phoneNumber)
+        self.btn_send = Button(text='Send', font_size=14)
+        self.add_widget(self.btn_send)
+        self.btn_send.bind(on_press=self.send)
+
+class MyApp(App):
+
     def build(self):
-        return Button(text='Hello World')
+        return Window()
 
-Window().run()
+
+if __name__ == '__main__':
+    MyApp().run()
